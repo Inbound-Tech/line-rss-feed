57a60f129837d46b42d1dac71e1ae67d5b464ef8
kenchang <workinkener@gmail.com>
2020-02-01 17:02:34 +0800

    remove articles

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import articles from './articles'
+// import articles from './articles'
 import feed from './feed'
 
 const router = Router()
 
-router.use('/articles', articles)
+// router.use('/articles', articles)
 router.use('/', feed)
 
 export default router
